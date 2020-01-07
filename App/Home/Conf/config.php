fc98ef4b236f8f6f8806637ad734bfa9f2478d5e
kitty168 <450038893@qq.com>
2020-01-07 14:54:45 +0800

    Update config.php

--- App/Home/Conf/config.php
+++ App/Home/Conf/config.php
@@ -5,7 +5,7 @@ return array(
     'DB_HOST'               =>  'localhost', // 服务器地址
     'DB_NAME'               =>  'choujiang',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  'Kitty1688??',          // 密码
+    'DB_PWD'                =>  'root',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'c_',    // 数据库表前缀
 
